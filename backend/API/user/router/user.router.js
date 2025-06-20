0b9be8ddab2648d1b2ecb6a4946529f4419e5dfa
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-06-20 18:34:58 +0530

    Adding multer

--- backend/API/user/router/user.router.js
+++ backend/API/user/router/user.router.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/user.controller");
+const multer = require("multer");
 
 // ========== ROUTES ========== //
 router.post("createUser", UserController.createUser);
