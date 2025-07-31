98eb18f87de5ef9ab3d3dd58afc5ff5e3feb52c3
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-07-31 13:11:28 +0530

    Adding the theme

--- client/src/components/layouts/homeLayout.jsx
+++ client/src/components/layouts/homeLayout.jsx
@@ -7,17 +7,17 @@ export const HomeLayout = () => {
     <>
       <div className="flex flex-col h-screen">
         {/* Sticky Header */}
-        <div className="sticky top-0 z-10">
+        <div className="sticky top-0 z-10 shadow-md">
           <HeaderNav />
         </div>
 
-        <div className="flex flex-1 overflow-hidden">
+        <div className="flex flex-1 overflow-hidden ps-2 bg-[#F9FAFB]">
           {/* Sidebar container */}
           <aside className="bg-[#F9FAFB] basis-[16rem] flex-shrink-0 h-full sticky top-[header-height] overflow-y-auto">
             <SidebarMenu />
           </aside>
           {/* Body Container */}
-          <div className="flex-grow overflow-y-auto p-5">
+          <div className="flex-grow md:order-2 overflow-y-auto p-5 bg-[#eeecec]">
             <div className="bg-white h-full overflow-y-scroll scrollbar-hide">
               <Outlet />
             </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,25 @@ export default {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2563EB", // Blue (Primary Action)
+          dark: "#1E40AF", // Darker Blue (Hover)
+        },
+        secondary: "#F3F4F6", // Light Gray (Background)
+        accent: "#0D9488",
+        emerald_green: "#10B981",
+        dark_gray: "#1F2937",
+        success: "#10B981", // Green (Success/Confirm)
+        warning: "#F59E0B", // Orange (Warning)
+        error: "#DC2626", // Red (Error/Danger)
+        dark: {
+          bg: "#1E293B", // Dark Mode Background
+          text: "#E5E7EB", // Light Text
+        },
+      },
+    },
   },
   plugins: [flowbite],
 };
