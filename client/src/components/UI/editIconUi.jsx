5dd9075f9923f9aae86a9cb1506dd1741c4da847
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-08-14 16:59:08 +0530

    Converting the edit, delete and view icon component

---
+++ client/src/components/UI/editIconUi.jsx
@@ -0,0 +1,9 @@
+import { MdEdit } from "react-icons/md";
+
+export const EditIcon = () => {
+  return (
+    <div className="p-1 hover:bg-blue-600 rounded-lg cursor-pointer">
+      <MdEdit className="hover:fill-white" />
+    </div>
+  );
+};
---
+++ client/src/components/UI/viewIconUi.jsx
@@ -0,0 +1,9 @@
+import { FaEye } from "react-icons/fa";
+
+export const ViewIcon = () => {
+  return (
+    <div className="p-1 hover:bg-green-600 rounded-lg cursor-pointer">
+      <FaEye className="hover:fill-white" />
+    </div>
+  );
+};
