6885486b3e088b3eb0564790d449f5e28335a5c8
Ajay Kumar <official.ajay2681993@gmail.com>
2025-08-15 22:48:14 +0530

    Creating Notification backend

---
+++ backend/API/notificationsManagement/model/notification.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Sequelize) => {
+  const tbl_notification_master = sequelize.define(
+    "NOTIFICATION_MASTER",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      archieve_status: {
+        type: Sequelize.ENUM("archieve", "non-archieve"),
+        allowNull: false,
+        defaultValue: "non-archieve",
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    { freezeTableName: true }
+  );
+  return tbl_notification_master;
+};
