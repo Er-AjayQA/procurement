b438c1bcc93c31ee0ecc778c2115d64a889ebdae
Ajay Kumar <official.ajay2681993@gmail.com>
2025-09-03 23:56:49 +0530

    Working on add calendar

--- client/src/components/employeeSelfService/calendar/calendarPage.jsx
+++ client/src/components/employeeSelfService/calendar/calendarPage.jsx
@@ -1,23 +1,92 @@
-import { CalendarListing } from "./calendarListing";
-import { CalendarEventForm } from "./calendarEventForm";
-import { CalendarView } from "./calendarView";
 import { useCalendarContext } from "../../../contextApis/useEssContextFile";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { CalendarEventForm } from "./calendarEventForm";
 
 export const CalendarPage = () => {
-  const { componentType, handleComponentView } = useCalendarContext();
+  const {
+    viewStyleOptions,
+    currentDate,
+    dateTab,
+    currentViewStyle,
+    handleViewStyleClick,
+    handleCurrentDate,
+    showEventForm,
+    daysOptions,
+  } = useCalendarContext();
 
   return (
     <>
       <div className="px-2 h-full">
-        {componentType === "listing" && (
-          <CalendarListing componentType={componentType} />
-        )}
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center px-3">
+            {/* Month Navigation */}
+            <div className="flex items-center gap-5">
+              <div className="flex items-center gap-2">
+                <button
+                  onClick={() => handleCurrentDate("prev")}
+                  className="p-2 rounded hover:bg-gray-100"
+                >
+                  <MdKeyboardArrowLeft size={20} />
+                </button>
+                <div className="font-bold">
+                  {currentDate.toLocaleString("default", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </div>
+                <button
+                  onClick={() => handleCurrentDate("next")}
+                  className="p-2 rounded hover:bg-gray-100"
+                >
+                  <MdKeyboardArrowRight size={20} />
+                </button>
+              </div>
+            </div>
+            {/* View Options */}
+            <div>
+              <ul className="flex gap-5">
+                {viewStyleOptions.map((view) => (
+                  <li
+                    key={view.value}
+                    className={`text-[.8rem] cursor-pointer font-bold p-2 rounded-md border border-transparent hover:text-white hover:border-gray-300 hover:bg-body-bg_color ${
+                      currentViewStyle === view.value
+                        ? "border-gray-300 bg-body-bg_color text-white"
+                        : ""
+                    }`}
+                    onClick={() => handleViewStyleClick(view.value)}
+                  >
+                    {view.label}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
 
-        {componentType === "form" && (
-          <CalendarEventForm onClose={() => handleComponentView("listing")} />
-        )}
+          {/* Days of Week Header */}
+          <div className="days_container mb-2">
+            {daysOptions?.map((day) => {
+              return (
+                <div
+                  key={day.value}
+                  className="text-center py-2 px-3 bg-blue-800 rounded-[10px]"
+                >
+                  <p className="text-white text-sm">
+                    {day?.label.substring(0, 3)}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
 
-        {componentType === "view" && <CalendarView />}
+        <div className="shadow-lg rounded-md border pb-2 border-gray-300 h-full flex flex-col overflow-auto scrollbar-hide">
+          <div className="dates_containers">
+            {dateTab.length > 0 ? dateTab : "No Records Found!"}
+          </div>
+        </div>
+
+        {/* Rendering the Add Event Form */}
+        {showEventForm && <CalendarEventForm />}
       </div>
     </>
   );
